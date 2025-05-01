e09a33e7a8548d8390673f1c6735035287f7f3aa
Vladimir <bizin.vladimir01@gmail.com>
2025-05-01 17:33:12 +0300

    initial commit: add gitignore, prepare project for laying out with code

---
+++ Tracker/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  Tracker
+//
+//  Created by Vladimir on 01.05.2025.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        print("in scene")
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+
+
+}
+
---
+++ Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tracker
+//
+//  Created by Vladimir on 01.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("viewDidLoad")
+        view.backgroundColor = .red
+    }
+
+
+}
+
