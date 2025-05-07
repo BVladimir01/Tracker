438012c9e249524433ca3a855c9e46d45ce27a8d
Vladimir <bizin.vladimir01@gmail.com>
2025-05-07 15:32:37 +0300

    refactor/restyle stats view controller

--- Tracker/StatsTab/StatsViewController.swift
+++ Tracker/StatsTab/StatsViewController.swift
@@ -9,38 +9,51 @@ import UIKit
 
 
 class StatsViewController: UIViewController {
+    
+    // MARK: - Private Properties
+    
+    private let stubView = UIView()
 
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Статистика"
-        addStub()
+        setUpStub()
     }
     
     // MARK: - Private Methods
     
-    private func addStub() {
+    private func setUpStub() {
         let stubImageView = UIImageView(image: .statsStub)
         stubImageView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(stubImageView)
+        stubView.addSubview(stubImageView)
         NSLayoutConstraint.activate([
-            stubImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            stubImageView.heightAnchor.constraint(equalToConstant: LayoutConstants.stubImageHeight),
-            stubImageView.widthAnchor.constraint(equalTo: stubImageView.heightAnchor, multiplier: LayoutConstants.stubImageAspectRatio),
-            stubImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: LayoutConstants.stubImageTopToSuperViewTop),
-            stubImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -LayoutConstants.stubImageBottomSuperViewBottom)
+            stubImageView.centerXAnchor.constraint(equalTo: stubView.centerXAnchor),
+            stubImageView.heightAnchor.constraint(equalToConstant: LayoutConstants.Stub.imageHeight),
+            stubImageView.widthAnchor.constraint(equalToConstant: LayoutConstants.Stub.imageWidth),
+            stubImageView.topAnchor.constraint(equalTo: stubView.topAnchor, constant: LayoutConstants.Stub.imageTopPadding)
         ])
         
         let label = UILabel()
         label.text = "Анализировать пока нечего"
-        label.font = UIFont.systemFont(ofSize: LayoutConstants.stubLabelFontSize, weight: LayoutConstants.stubLabelFontWeight)
-        label.textColor = .ypBlack
+        label.font = LayoutConstants.Stub.labelFont
+        label.textColor = LayoutConstants.Stub.textColor
         label.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(label)
+        stubView.addSubview(label)
         NSLayoutConstraint.activate([
-            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            label.topAnchor.constraint(equalTo: stubImageView.bottomAnchor, constant: LayoutConstants.stubLabelTopToStubImageBottom)
+            label.centerXAnchor.constraint(equalTo: stubView.centerXAnchor),
+            label.topAnchor.constraint(equalTo: stubImageView.bottomAnchor, constant: LayoutConstants.Stub.labelToImageSpacing)
+        ])
+        
+        stubView.backgroundColor = .ypWhite
+        stubView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(stubView)
+        NSLayoutConstraint.activate([
+            stubView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            stubView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            stubView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            stubView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
     }
 
@@ -50,13 +63,14 @@ class StatsViewController: UIViewController {
 // MARK: - LayoutConstants
 extension StatsViewController {
     private enum LayoutConstants {
-        static let stubImageHeight: CGFloat = 80
-        static let stubImageAspectRatio: CGFloat = 1
-        static let stubImageTopToSuperViewTop: CGFloat = 375
-        static let stubImageBottomSuperViewBottom: CGFloat = 357
-        
-        static let stubLabelFontSize: CGFloat = 12
-        static let stubLabelFontWeight: UIFont.Weight = .medium
-        static let stubLabelTopToStubImageBottom: CGFloat = 8
+        enum Stub {
+            static let imageHeight: CGFloat = 80
+            static let imageWidth: CGFloat = 80
+            static let imageTopPadding: CGFloat = 220
+            
+            static let labelFont: UIFont = .systemFont(ofSize: 12, weight: .medium)
+            static let textColor: UIColor = .ypBlack
+            static let labelToImageSpacing: CGFloat = 8
+        }
     }
 }
