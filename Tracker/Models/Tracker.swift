fc51ad41c4e2bef687896bbbda78a16a1e26db4c
Vladimir <bizin.vladimir01@gmail.com>
2025-05-08 22:08:53 +0300

    make tracker hashable

--- Tracker/Models/Tracker.swift
+++ Tracker/Models/Tracker.swift
@@ -7,12 +7,20 @@
 
 import Foundation
 
-struct Tracker: Identifiable {
+struct Tracker: Identifiable, Equatable, Hashable {
     
     let id: UUID
     let title: String
     let color: RGBColor
     let emoji: Character
     let schedule: Schedule
+    
+    static func == (lhs: Tracker, rhs: Tracker) -> Bool {
+        lhs.id == rhs.id
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
     
 }
