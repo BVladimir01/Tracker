e75ad4538e3dd41d7d64a82dbd78d7ddd78b4f7a
Vladimir <bizin.vladimir01@gmail.com>
2025-05-10 12:54:16 +0300

    feat(weekday): add methods and properties for convenience

--- Tracker/Models/Weekday.swift
+++ Tracker/Models/Weekday.swift
@@ -9,4 +9,26 @@ import Foundation
 
 enum Weekday: Int, CaseIterable, Equatable {
     case sunday = 1, monday, tuesday, wednesday, thursday, friday, saturday
+    
+    static let week: [Weekday] = [.monday, .tuesday, .wednesday, .thursday, .friday, .saturday, .sunday]
+    
+    func asString(short: Bool) -> String {
+        switch self {
+        case .sunday:
+            short ? "Вс" : "Воскресенье"
+        case .monday:
+            short ? "Пн" : "Понедельник"
+        case .tuesday:
+            short ? "Вт" : "Вторник"
+        case .wednesday:
+            short ? "Ср" : "Среда"
+        case .thursday:
+            short ? "Чт" : "Четверг"
+        case .friday:
+            short ? "Пт" : "Пятница"
+        case .saturday:
+            short ? "Сб" : "Суббота"
+        }
+    }
+    
 }
