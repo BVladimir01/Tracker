4a3d7a970297c2c11290c8d393a2da3d0ec35b56
Vladimir <bizin.vladimir01@gmail.com>
2025-06-02 09:48:03 +0300

    add delegate to TrackerCategoryStore, implement methods for notifying data change

--- Tracker/DataBase/TrackerCategoryStore.swift
+++ Tracker/DataBase/TrackerCategoryStore.swift
@@ -9,13 +9,25 @@
 import CoreData
 
 
-final class TrackerCategoryStore {
+struct TrackerCategoryUpdate {
+    let insertedIndices: IndexSet
+}
+
+protocol TrackerCategoryStoreDelegate {
+    func didUpdate(with update: TrackerCategoryUpdate)
+}
+
+final class TrackerCategoryStore: NSObject {
     
     private let context: NSManagedObjectContext
     private let fetchedResultsController: NSFetchedResultsController<TrackerCategoryEntity>
+    private let delegate:TrackerCategoryStoreDelegate
     
-    init(context: NSManagedObjectContext) throws {
+    private var insertedIndices: IndexSet?
+    
+    init(context: NSManagedObjectContext, delegate: TrackerCategoryStoreDelegate) throws {
         self.context = context
+        self.delegate = delegate
         let request = TrackerCategoryEntity.fetchRequest()
         request.sortDescriptors = [NSSortDescriptor(keyPath: \TrackerCategoryEntity.title, ascending: true)]
         let resultsController =  NSFetchedResultsController(fetchRequest: request,
@@ -30,6 +42,37 @@ final class TrackerCategoryStore {
         let categoryEntity = TrackerCategoryEntity(context: context)
         categoryEntity.title = category.title
         try context.save()
+    }
+    
+}
+
+
+extension TrackerCategoryStore: NSFetchedResultsControllerDelegate {
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<any NSFetchRequestResult>) {
+        insertedIndices = IndexSet()
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<any NSFetchRequestResult>) {
+        guard let insertedIndices else {
+            assertionFailure("TrackerCategoryStore.controllerDidChangeContent: changed indices are nil on update")
+            return
+        }
+        let update = TrackerCategoryUpdate(insertedIndices: insertedIndices)
+        delegate.didUpdate(with: update)
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<any NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch type {
+        case .insert:
+            guard let newIndexPath else {
+                assertionFailure("TrackerCategoryStore.controller: Failed to unwrap newIndexPath for insertion")
+                return
+            }
+            insertedIndices?.insert(newIndexPath.item)
+        default:
+            break
+        }
     }
     
 }
